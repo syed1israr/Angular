9319a77d423c9ebd7e3ac72605c0aacb66263654
Israr <syedfaraz173@gmail.com>
2025-01-21 10:43:23 +0530

    Intial Commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from "./header/header.component";
+import { FooterComponent } from "./footer/footer.component";
+import { SectionComponent } from "./section/section.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, HeaderComponent, FooterComponent, SectionComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
